d45548481af0438ab79b4f50fa2ff4461b07878e
Sahil Sorathiya <63040858+8999456115@users.noreply.github.com>
2025-06-19 19:07:30 +0000

    Final flake8 cleanup: spacing fixes

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv(f'.env.{os.getenv("ENV")}')
 
 app = Flask(__name__)
+app.config['DEBUG'] = os.getenv('DEBUG', 'False') == 'True'
+
 
 @app.route('/')
 def hello():
-    return "Hello from CI Pipeline!"
+    return f"Hello from CI Pipeline! Debug Mode: {app.config['DEBUG']}"
+
 
 if __name__ == '__main__':
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,21 @@
 from app import app
 
+
 def test_home_status_code():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
+
 
 def test_home_data():
     tester = app.test_client()
     response = tester.get('/')
     assert b"Hello from CI Pipeline!" in response.data
 
+
 def test_app_exists():
     assert app is not None
+
 
 def test_app_debug_mode():
     assert app.debug is False
